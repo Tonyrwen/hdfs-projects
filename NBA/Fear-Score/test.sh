a3f4d196b51ba75bdd3dc5e797b405a7316fcab5
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-03-28 23:39:57 -0400

    Update test.sh

--- NBA/Fear-Score/test.sh
+++ NBA/Fear-Score/test.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 /usr/local/hadoop/sbin/start-all.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /fs/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /fs/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /fs/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Fear-Score/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Fear-Score/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Fear-Score/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../shot_logs.csv /fs/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../fs/mapper1.py -mapper ../fs/mapper1.py \
--file ../fs/reducer1.py -reducer ../fs/reducer1.py \
--input /fs/input/* -output /fs/output/
+-file ../Fear-Score/mapper1.py -mapper ../Fear-Score/mapper1.py \
+-file ../Fear-Score/reducer1.py -reducer ../Fear-Score/reducer1.py \
+-input /Fear-Score/input/* -output /Fear-Score/output/
 
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
@@ -19,13 +19,13 @@
 -D mapred.text.key.comparator.options="-k1,1 -k2,2n -k3,3nr"\
 # partition by player name only
 -D mapred.text.key.partitioner.options=-k1,1 \
--file ../fs/mapper2.py -mapper ../fs/mapper2.py \
--file ../fs/reducer2.py -reducer ../fs/reducer2.py \
--input /fs/output/* -output /fs-2/output/
+-file ../Fear-Score/mapper2.py -mapper ../Fear-Score/mapper2.py \
+-file ../Fear-Score/reducer2.py -reducer ../Fear-Score/reducer2.py \
+-input /Fear-Score/output/* -output /Fear-Score-2/output/
 
-/usr/local/hadoop/bin/hdfs dfs -cat /fs-2/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /fs/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /fs/output/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /fs-2/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /Fear-Score-2/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Fear-Score/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Fear-Score/output/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Fear-Score-2/output/
 /usr/local/hadoop/sbin/stop-all.sh
 
