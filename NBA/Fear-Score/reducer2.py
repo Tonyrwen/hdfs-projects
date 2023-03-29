e02316473b9dc09d716b698f2733ec0d5d4c2540
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-03-29 00:11:05 -0400

    Update reducer2.py

--- NBA/Fear-Score/reducer2.py
+++ NBA/Fear-Score/reducer2.py
@@ -5,19 +5,17 @@ dct = {}
 
 for line in sys.stdin:
 
-  player, hit_rate, attempt, defender = line.strip().split(",")
+    player, hit_rate, attempt, defender = line.strip().split(",")
 
-  current_player = dct.get(player, {0:[0,[]]})
-  current_stat = current_player.get(hit_rate, [[],[]])
-  try:
-    current_stat[0].append(int(attempt))
-    current_stat[1].append(defender)
-    current_player[hit_rate] = current_stat
-    dct[player] = current_player
-  except ValueError: pass
+    current_player = dct.get(player, {0:[0,[]]})
+    current_stat = current_player.get(hit_rate, [[],[]])
+    try:
+        current_stat[0].append(int(attempt))
+        current_stat[1].append(defender)
+        current_player[hit_rate] = current_stat
+        dct[player] = current_player
+    except ValueError: pass
 
 for key, value in dct.items():
-  #print(key, value)
-  #print(list(list(value.items())[1]))
-  worst_stat = list(value.items())[1]
-  print("PLAYER: %s, DEFENDERS: %s\t\tATTEMPTS: %s\tACCURACY: %s"%(key, worst_stat[1][1], worst_stat[1][0], worst_stat[0]))
+    worst_stat = list(value.items())[1]
+    print("PLAYER: %s, DEFENDERS: %s\t\tATTEMPTS: %s\tACCURACY: %s"%(key, worst_stat[1][1], worst_stat[1][0], worst_stat[0]))
