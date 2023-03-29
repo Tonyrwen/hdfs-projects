bde00b7e0eef9423035270521c6bfed6b43f83f3
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-03-29 00:05:34 -0400

    Update mapper1.py

--- NBA/Fear-Score/mapper1.py
+++ NBA/Fear-Score/mapper1.py
@@ -2,17 +2,17 @@
 import sys
 
 for idx, line in enumerate(sys.stdin):
-  if idx == 0: continue
+    if idx == 0: continue
 
-  line = line.strip().split(",")
+    line = line.strip().split(",")
 
-  def_last, def_first, player = line[15].lower().strip(), line[16].lower().strip(), line[21].replace(" ", "_") 
+    def_last, def_first, player = line[15].lower().strip(), line[16].lower().strip(), line[21].replace(" ", "_") 
   
-  made = 1 if line[14] == "made" else 0 if line[14] == "missed" else None
+    made = 1 if line[14] == "made" else 0 if line[14] == "missed" else None
   
-  defender = (' '.join((def_first,def_last))).replace('"', "").replace(" ", "_")
+    defender = (' '.join((def_first,def_last))).replace('"', "").replace(" ", "_")
 
-  print("%s,%s\t%s,%s"%(player, defender, made, 1))
+    print("%s,%s\t%s,%s"%(player, defender, made, 1))
 
 
 
