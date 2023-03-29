2f521a5a9881a5e6ac189c939402904875474a79
Tonyrwen <47587273+Tonyrwen@users.noreply.github.com>
2023-03-29 00:10:16 -0400

    Update reducer1.py

--- NBA/Fear-Score/reducer1.py
+++ NBA/Fear-Score/reducer1.py
@@ -4,20 +4,19 @@ import sys
 pair_dct = {}
 
 for line in sys.stdin:
-  
-  pair, shot = line.strip().split("\t")
-  made, attempt = shot.split(",")
+    pair, shot = line.strip().split("\t")
+    made, attempt = shot.split(",")
 
-  current = pair_dct.get(pair, [0,0])
-  try:
-    current[0] += int(made)
-    current[1] += int(attempt)
-    pair_dct[pair] = current
-  except ValueError: pass
+    current = pair_dct.get(pair, [0,0])
+    try:
+        current[0] += int(made)
+        current[1] += int(attempt)
+        pair_dct[pair] = current
+    except ValueError: pass
 
 for key, value in pair_dct.items():
-  if value[1] < 10: continue
-  player, defender = key.split(",")
-  hit_rate = float(value[0])/value[1]
+    if value[1] < 10: continue
+    player, defender = key.split(",")
+    hit_rate = float(value[0])/value[1]
 
-  print("%s,%s,%s,%s"%(player, hit_rate, value[1], defender))
+    print("%s,%s,%s,%s"%(player, hit_rate, value[1], defender))
